e642f6a909e6d8d5a1a1791c22bcdc32933697a7
Alan Cleyton <alancleyton67@gmail.com>
2024-06-18 15:40:52 -0300

    chore: add media root to django settings

--- djangocrm/settings.py
+++ djangocrm/settings.py
@@ -126,6 +126,10 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / 'static'
 
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
