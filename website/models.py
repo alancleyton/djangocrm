6c2b5fa34669729f41d978f6b23433b01697d713
Alan Cleyton <alancleyton67@gmail.com>
2024-06-18 12:05:59 -0300

    feat: create and register website Customer model

--- website/models.py
+++ website/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    phone = models.CharField(max_length=30)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
