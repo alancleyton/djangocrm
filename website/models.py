c0120166ba90135f851fe17ea0fb6acf6337db34
Alan Cleyton <alancleyton67@gmail.com>
2024-06-20 14:33:40 -0300

    feat: add owner field to customer model

--- website/models.py
+++ website/models.py
@@ -2,6 +2,7 @@
 # pylint: disable=missing-class-docstring
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Company(models.Model):
     name = models.CharField(max_length=50)
@@ -21,12 +22,8 @@ class Customer(models.Model):
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(default=timezone.now)
     photo = models.ImageField(upload_to='images/', blank=True, null=True)
-    company = models.ForeignKey(
-        Company,
-        blank=True,
-        null=True,
-        on_delete=models.SET_NULL,
-    )
+    company = models.ForeignKey(Company, blank=True, null=True, on_delete=models.SET_NULL)
+    owner = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
