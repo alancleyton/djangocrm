92a8b121675e7e3a85017d7fcbe1449fcb5f7ffc
Alan Cleyton <alancleyton67@gmail.com>
2024-06-26 10:39:38 -0300

    feat: add order query to customers list

--- website/views/customers.py
+++ website/views/customers.py
@@ -3,6 +3,6 @@ from django.http import HttpRequest, HttpResponse
 from website.models import Customer as CustomerModel
 
 def list_customers(request: HttpRequest) -> HttpResponse:
-    customers = CustomerModel.objects.all()
+    customers = CustomerModel.objects.all().order_by('-id')
     context = { 'customers': customers }
     return HttpResponse(render(request, 'index.html', context))
