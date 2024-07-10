c5b82a891100d8d447cf1a6f183c094a677c5289
Alan Cleyton <alancleyton67@gmail.com>
2024-07-10 11:41:07 -0300

    fix: adjust customers url pattern

--- website/urls.py
+++ website/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from website.views import customers as customers_views
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
-    path('', customers_views.list_customers, name='customers'),
-    path('<int:customer_id>/', customers_views.show_customer, name='customer'),
+    path('', lambda r: HttpResponseRedirect('customers')),
+    path('customers', customers_views.list_customers, name='customers'),
+    path('customers/<int:customer_id>/details', customers_views.show_customer, name='customer'),
     path('search/', customers_views.search_customers, name='search'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
